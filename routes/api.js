effa46f1ce8abaac2f6efc5201451c4d735e3c79
yk <yangk802@163.com>
2021-11-21 22:48:11 +0800

    version 1.0.1

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/test', function(req, res, next) {
+  res.send({
+    code: 200,
+    msg: '测试成功'
+  })
 });
 
 
