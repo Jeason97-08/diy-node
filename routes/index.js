5f6e5784466b9805da86c3c7c9852fb380d63fbd
Jeason <458840890@qq.com>
2021-11-22 09:53:41 +0800

    add wx auth

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//微信接入验证
+router.get('/auth', function(req, res, next) {
+  let {signature,timestamp,nonce,echostr} = req.query
+  let token = 'wxTestToken'
+  let arr = [token,timestamp,nonce]
+  arr.sort()
+  const sha1 = require('sha1')
+  let encodeStr = sha1(arr.join('')) 
+  if(encodeStr == signature){
+    res.send(echostr)
+  }else{
+    res.send(false)
+  }
+});
+
 
 module.exports = router;
